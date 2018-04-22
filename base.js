b7dac0728be153a3c6c615c0f272b3ded500eb45
jamietsang <zengjm@innapp.cn>
2018-04-22 23:57:26 +0800

    正式提交

---
+++ base.js
@@ -0,0 +1,6 @@
+var base = 'base';
+
+let haha = '123';
+
+let change = 'fixed bug';
+let change2 = 'fixed bug2';
---
+++ work.js
@@ -0,0 +1,20 @@
+let xiexie = 'xiexiexie';
+var xiexie = 'xiexiexie';
+var xiexie = 'xiexiexie';
+var xiexie = 'xiexiexie';
+var xiexie = 'xiexiexie';
+var xiexie = 'xiexiexie';
+
+console.log('这里上面也是很正常的代码');
+console.log('分叉1');
+console.log('分叉1');
+console.log('分叉1');
+console.log('分叉1');
+console.log('分叉1');
+console.log('分叉1add');
+console.log('分叉1add');
+console.log('分叉1add');
+console.log('分叉1add');
+console.log('分叉1add');
+
+console.log(xiexie);
