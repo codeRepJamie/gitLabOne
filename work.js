47a7bca07eeb4668463aeb3c9f86d3335cae69e8
jamietsang <zengjm@innapp.cn>
2018-04-22 21:46:22 +0800

    大胆的写发2

--- work.js
+++ work.js
@@ -8,5 +8,7 @@ var xiexie = 'xiexiexie';
 console.log('这里上面也是很正常的代码');
 console.log('写了一个大胆的东西不知道行不行');
 console.log('写了一个大胆的东西不知道行不行');
+console.log('写了一个大胆的东西不知道行不行');
+console.log('写了一个大胆的东西不知道行不行');
 
 console.log(xiexie);
